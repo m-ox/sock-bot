d1fe01f7136dffccc633453ab9ca40a6b7274c61
Maudlin Oxalis <maudlin.oxalis@gmail.com>
2025-10-20 01:01:09 -0600

    maybe I was wrong

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,8 @@
-export default function ready(client) {
-  console.log(`✅ Logged in as ${client.user.tag}`);
-  client.user.setPresence({
-    activities: [{ name: "lurking", type: 0 }],
-    status: "online",
-  });
+import ready from "./ready.js";
+import messageCreate from "./messageCreate.js";
+import { Events } from "discord.js";
+
+export function registerEvents(client) {
+  client.once(Events.ClientReady, () => ready(client));
+  client.on(Events.MessageCreate, (message) => messageCreate(message, client));
 }
